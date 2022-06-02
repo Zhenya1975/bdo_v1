977287fcc551f7722ff498b3cf2b62a1845edecd
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-06-02 09:38:42 +0000

    удаление записи из мастер-файла ео

--- func.py
+++ func.py
@@ -1,7 +1,9 @@
-from functions import import_sap_eo_data, import_be_data, read_sap_eo_xlsx_file
+from functions import import_sap_eo_data, import_be_data, read_sap_eo_xlsx_file, delete_records_from_eo_master_data
 
 # import_sap_eo_data.update_sap_eo_data()
 
 # import_be_data.update_sap_be_data()
 
 read_sap_eo_xlsx_file.read_sap_eo_xlsx()
+
+# delete_records_from_eo_master_data.delete_from_eo_master_data()
--- functions/read_sap_eo_xlsx_file.py
+++ functions/read_sap_eo_xlsx_file.py
@@ -1,7 +1,10 @@
 import pandas as pd
+from extensions import extensions
 from models.models import Eo_DB, Be_DB
 from initial_values.initial_values import sap_columns_to_master_columns
 from app import app
+
+db = extensions.db
 
 sap_columns_to_master_columns = sap_columns_to_master_columns
 # sap_columns_to_master_columns = {'Единица оборудования': 'eo_code', 'Название технического объекта':'eo_description', 'Техническое место': 'teh_mesto','Поле сортировки':'gar_no'}
@@ -9,11 +12,26 @@ sap_columns_to_master_columns = sap_columns_to_master_columns
 def read_sap_eo_xlsx():
   with app.app_context():
     sap_eo_raw_data = pd.read_excel('uploads/sap_eo_data.xlsx', index_col = False, dtype=str)
+    
     sap_eo_data = sap_eo_raw_data.rename(columns=sap_columns_to_master_columns)
-    # читаем мастер-файл из базы
-    eo_master_data=Eo_DB.query.all()
+    # итерируемся по полученному файлу
+    for row in sap_eo_data.itertuples():
+      # получаем eo_code
+      eo_code = getattr(row, "eo_code")
+      # print("eo_code в файле excel из сап ", eo_code)
+      # читаем мастер-файл из базы
+      eo_master_data=Eo_DB.query.filter_by(eo_code=eo_code).first()
+      # если данных нет, то добавляем запись. Если данные есть, то будем далее обновлять
+      if eo_master_data == None:
+        new_eo_master_data_record = Eo_DB(eo_code=eo_code)
+        db.session.add(new_eo_master_data_record)
+        db.session.commit()
+        print('в мастер-файл добавлена новая запись с eo: ', eo_code)
 
-  
+        
+    
+
+
 
   # переименовываем колонки из sap наименований в master
   
