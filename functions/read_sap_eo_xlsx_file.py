3ce6b989dcb64cd45966ed4ac30f2d2b6a3b575a
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-06-02 07:16:48 +0000

    добавлена проверка по имени файла

---
+++ functions/read_sap_eo_xlsx_file.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def read_sap_eo_xlsx():
+  sap_eo_raw_data = pd.read_excel('uploads')
--- routes/routes.py
+++ routes/routes.py
@@ -24,7 +24,7 @@ def home_view():
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
-  ALLOWED_EXTENSIONS = {'csv', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}  
+  ALLOWED_EXTENSIONS = {'xlsx','csv'}  
   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @home.route('/upload', methods=['GET', 'POST'])
@@ -42,7 +42,15 @@ def upload_file():
       message = f"Неразрешенное расширение файла {uploaded_file.filename}"
       flash(message, 'alert-danger')
       return redirect(url_for('home.home_view'))
-    else:  
+    elif "sap_eo_data" not in uploaded_file.filename:
+      message = "В имени файла нет текста sap_eo_data"
+      flash(message, 'alert-danger')    
+      return redirect(url_for('home.home_view'))
+    elif "xlsx" not in uploaded_file.filename:
+      message = "В имени файла нет расширения xlsx"
+      flash(message, 'alert-danger')    
+      return redirect(url_for('home.home_view'))
+    else:    
       uploaded_file.save(os.path.join('uploads', uploaded_file.filename))
       message = f"файл {uploaded_file.filename} загружен"    
       flash(message, 'alert-success')
