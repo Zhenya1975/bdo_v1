5f89ee565f52360cfc0921ca295b97413a0153a5
Zhenya <57045129+Zhenya1975@users.noreply.github.com>
2022-06-08 14:59:26 +0000

    отходим от csv. бэкапим бд

--- func.py
+++ func.py
@@ -1,6 +1,5 @@
-from functions import import_sap_eo_data, import_be_data, read_sap_eo_xlsx_file, delete_records_from_eo_master_data
+from functions import backup_restore_bd
 
 
-# read_sap_eo_xlsx_file.read_sap_eo_xlsx()
+backup_restore_bd.backup_bd()
 
-# delete_records_from_eo_master_data.delete_from_eo_master_data()
--- migrations/versions/18700277aa85_init.py
+++ migrations/versions/18700277aa85_init.py
@@ -1,8 +1,8 @@
 """init
 
-Revision ID: 9a17ea0d7ef2
+Revision ID: 18700277aa85
 Revises: 
-Create Date: 2022-06-08 13:47:53.005744
+Create Date: 2022-06-08 14:58:34.155201
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9a17ea0d7ef2'
+revision = '18700277aa85'
 down_revision = None
 branch_labels = None
 depends_on = None
